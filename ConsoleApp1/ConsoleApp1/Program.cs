c9d0363f5f9dfc3bcc52e68ad209c7e7f6350bfc
LIAM MORGAN <2203912@uad.ac.uk>
2023-09-27 12:21:56 +0100

    Add writeline indicating we are in cmd line

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("I just added this in visual studio");
+Console.WriteLine("I just added this in the cmd line repo");
