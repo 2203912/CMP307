77fd0706d66af6accac04423522b500b7dd95134
LIAM MORGAN <2203912@uad.ac.uk>
2023-10-04 12:39:05 +0100

    Add multiply method

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -16,5 +16,13 @@ namespace ConsoleApp1
         {
             return left - right;
         }
+        public int Multiply(int left, int right)
+        {
+            return left * right;
+        }
+        public int Divide (int left, int right)
+        {
+            return left / right;
+        }
     }
 }
